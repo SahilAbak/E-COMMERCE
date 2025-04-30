7f4fd793e6c236bc2925ed2fe42307b2057255ed
Sahil <sahilabak45@gmail.com>
2025-04-30 09:56:59 +0530

    Upload Again1

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,4 +1,6 @@
 import { serve } from "inngest/next";
+import { getAuth } from "@clerk/nextjs/dist/types/server";
+
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 
